37b94bb833c69ff1fcd62e0f6dc2e64369c41720
Coleraines13 <nttreaper@gmail.com>
2023-01-11 14:21:02 -0800

    fixed for checks

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ static void Main(string[] args)
 {
 double percent = .7553;
 double currency = 98765.4321;
-console.WriteLine("Percent: {0:0.00%}", percent);
+Console.WriteLine("Percent: {0:0.00%}", percent);
 Console.WriteLine("Currency: {0:C}", currency);
 }
 }
