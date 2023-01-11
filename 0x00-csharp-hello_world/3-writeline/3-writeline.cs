a87076fb5d8ea7eba452b912ff6b4a7fbdd3b0eb
Coleraines13 <nttreaper@gmail.com>
2023-01-11 19:16:48 +0000

    added 4 file

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -6,7 +6,7 @@ class Program
 {
 static void Main(string[] args)
 {
-Console.WriteLine("\"Programming is like building a multilingual puzzel");
+Console.WriteLine("\"Programming is like building a multilingual puzzle");
 }
 }
 }
