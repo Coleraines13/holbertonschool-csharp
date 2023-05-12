20b48206408a8fd3185c529328be8a0efc2662b0
Coleraines13 <nttreaper@gmail.com>
2023-05-11 20:04:53 -0700

    added the rest 6-13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+	public static void Delete(LinkedList<int> myLList, int index)
+	{
+		try
+		{
+			int val = myLList.Select((item, inx) => new { item, inx }).First(el => el.inx == index).item;
+			myLList.Remove(val);
+		}
+
+		catch (Exception)
+		{ }
+	}
+}
