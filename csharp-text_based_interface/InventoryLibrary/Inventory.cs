e2be4cabb8823c2614a48e89799d7047a2347f07
Coleraines13 <nttreaper@gmail.com>
2023-05-30 19:57:04 -0700

    item, user, inventory

---
+++ csharp-text_based_interface/InventoryLibrary/Inventory.cs
@@ -0,0 +1,54 @@
+namespace InventoryLibrary
+{
+	public class Inventory : BaseClass
+	{
+		private string _user_id;
+
+		public string user_id
+		{
+			get { return _user_id; }
+			set
+			{
+				updatetime();
+				_user_id = value;
+			}
+		}
+
+		private string _item_id;
+
+		public string item_id
+		{
+			get { return _item_id; }
+			set
+			{
+				updatetime();
+				_item_id = value;
+			}
+		}
+
+		private int _quantity;
+
+		public int quantity
+		{
+			get { return _quantity; }
+			set
+			{
+				updatetime();
+				if (value < 0) value = 0;
+				_quantity = value;
+			}
+		}
+		/// <summary>
+		/// Inventory
+		/// </summary>
+		/// <param name="user">user</param>
+		/// <param name="item">item</param>
+		/// <param name="quantity">quantity</param>
+		public Inventory(User user, Item item, int quantity = 1)
+		{
+			user_id = user.id;
+			item_id = item.id;
+			this.quantity = quantity;
+		}
+	}
+}
